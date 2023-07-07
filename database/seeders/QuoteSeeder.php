ed9b2e035c4c79dc9d15674fc3b020e8120ebbdf
satianferdy <satianferdy@gmail.com>
2023-07-07 20:58:48 +0700

    setup model, controller and others

---
+++ database/seeders/QuoteSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Quote;
+
+class QuoteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        Quote::factory()->count(10)->create();
+    }
+}
